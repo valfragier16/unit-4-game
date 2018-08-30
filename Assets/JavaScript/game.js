9dc9ab6da07ca7b93bfdf840c301e92cf62c5d7d
Valerie <valfragier@gmail.com>
2018-08-30 13:44:36 -0400

    final

--- Assets/JavaScript/game.js
+++ Assets/JavaScript/game.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
     ----------------------------------------------- */
 
 
-    //Define variables: Set Each crystal to display random number when selected
+    //Define variables: Set Each crystal to display random hidden value between 1-12
 	var crystalOne = Math.floor(Math.random() * 12) + 1; 
 		console.log("Crystal 1: " + crystalOne); 
 		$("#img1").html("<img src=" + "assets/images/image1.png" + " value=" + crystalOne + ">"); 
@@ -53,59 +53,56 @@ $(document).ready(function() {
 		console.log("score: " + score); 
     
 
-    // 1) Computer picks random nummber that is automatically displayed on page to begin game
+    // 1) Computer picks random nummber that is between 19 - 120, automatically displayed on page to begin game
     for (var compPick=19; compPick <= 120; compPick++){
         var compPick = Math.floor(Math.random() * 102) + 19; 
         console.log("compPick: " + compPick); 
         $(".randomNumber").html(compPick); 
     }
 
-        // Game begins - score and cyrstal random number generator 0
-        function reset () {
-            compPick = Math.floor(Math.random() * 102) + 19; 
-                console.log("compPick: " + compPick); 
-            $(".randomNumber").html(compPick); 
+    function reset () {
+        score = 0; 
+        $(".scoreDisplay").html(score); 
     
-            score = 0; 
-            $(".scoreDisplay").html(score); 
+        crystalOne = Math.floor(Math.random() * 12) + 1;  
+            console.log("Crystal 1: " + crystalOne); 
+        $("#img1").html("<img src=" + "Assets/Images/image1.png" + " value=" + crystalOne + ">"); 
     
-            crystalOne = Math.floor(Math.random() * 12) + 1;  
-                console.log("Crystal 1: " + crystalOne); 
-            $("#img1").html("<img src=" + "Assets/Images/image1.png" + " value=" + crystalOne + ">"); 
+        crystalTwo = Math.floor(Math.random() * 12) + 1; 
+            console.log("Crystal 2: " + crystalTwo); 
+        $("#img2").html("<img src=" + "assets/images/image2.png" + " value=" + crystalTwo + ">"); 
     
-            crystalTwo = Math.floor(Math.random() * 12) + 1; 
-                console.log("Crystal 2: " + crystalTwo); 
-            $("#img2").html("<img src=" + "assets/images/image2.png" + " value=" + crystalTwo + ">"); 
-    
-            crystalThree = Math.floor(Math.random() * 12) + 1; 
-                console.log("Crystal 3: " + crystalThree); 
-            $("#img3").html("<img src=" + "assets/images/image3.png" + " value=" + crystalThree + ">");
+        crystalThree = Math.floor(Math.random() * 12) + 1; 
+            console.log("Crystal 3: " + crystalThree); 
+        $("#img3").html("<img src=" + "assets/images/image3.png" + " value=" + crystalThree + ">");
         
-            crystalFour = Math.floor(Math.random() * 12) + 1; 
-                console.log("Crystal 4: " + crystalFour); 
-            $("#img4").html("<img src=" + "assets/images/image4.png" + " value=" + crystalFour + ">");
+        crystalFour = Math.floor(Math.random() * 12) + 1; 
+            console.log("Crystal 4: " + crystalFour); 
+        $("#img4").html("<img src=" + "assets/images/image4.png" + " value=" + crystalFour + ">");
     
-            $("img").on("click", function () {
-                var newScore = score += parseInt($(this).attr("value")); 
-                    console.log("New Score: " + newScore); 
-                $(".scoreDisplay").html(newScore); 
-    
-                if(newScore === compPick) { 
-                    wins++ ; 
-                    $(".wins").html("Wins: " + wins); 
-                        console.log("Wins: " + wins); 
-                        reset(); 
-                } 
-    
-                else if(newScore > compPick) {
-                    losses++ ; 
-                    $(".losses").html("Losses: " + losses); 
-                        console.log("Losses: " + losses); 
-                        reset(); 
-                    
-                }
-            }); 
-        }
+
+    //Display crystals as buttons
+        $("img").on("click", function () {
+            var newScore = score += parseInt($(this).attr("value")); 
+                console.log("New Score: " + newScore); 
+            $(".scoreDisplay").html(newScore); 
+
+            if(newScore === compPick) { 
+                wins++ ; 
+                $(".wins").html("Wins: " + wins); 
+                    console.log("Wins: " + wins); 
+                    reset(); 
+            } 
+
+            else if(newScore > compPick) {
+                losses++ ; 
+                $(".losses").html("Losses: " + losses); 
+                    console.log("Losses: " + losses); 
+                    reset(); 
+                
+            }
+        }); 
+    }
         
 
 
